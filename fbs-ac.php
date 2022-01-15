0c5431262be52d67ed885dd62483bf24323ddc30
Fazle Bari <fazlebarisn@gmail.com>
2022-01-15 15:38:44 +0600

    create instance fun

--- fbs-ac.php
+++ fbs-ac.php
@@ -1,84 +1,32 @@
 <?php
 /**
- * @package KbsPlugin
-*/
-
-/*
 Plugin Name: Fbs Ac
 Plugin URI: https://www.chitabd.com/
-Description: New plugin to do something
+Description: New plugin to create a address book
 Version: 1.0.0
 Author: Fazle Bari
 Author URI: https://www.chitabd.com/
 Licence: GPL Or leater
-Text Domain: fbs
-*/
-
-/*
-This is a free plugin
+Text Domain: fbsac
 */
 
 defined('ABSPATH') or die('Nice Try!');
 
-function fbs_user_contact_methods($methods){
-    $methods['twitter'] = __('Twitter' , 'fbs');
-    $methods['facebook'] = __('Facebook' , 'fbs');
-    return $methods;
-}
+/**
+ * The main class
+ */
 
-add_filter('user_contactmethods','fbs_user_contact_methods');
+ final class FbsAc{
 
-function fbs_author_contact_info($content){
+     public static function init(){
 
-    global $post;
+         static $instance = false;
 
-    $author_id = $post->post_author;
-    $author = get_user_by( 'id' , $author_id );
-    //var_dump($author_name);
-    $twitter = get_user_meta($author->ID,'twitter',true);
-    $facebook = get_user_meta($author->ID,'facebook',true);
-    $bio = get_user_meta($author->ID,'description',true);
+         if( !$instance ){
+             $instance = new self();
+         }
 
-    ob_start();
+         return $instance;
+     }
 
-    // Action hook
-    $name = "Sony";
-    $age = 30;
-    do_action('fbs_ac_bio_content_top' , $name, $age );
-
-    ?>
-        <div>
-            <?php
-                // filter hook
-                $your_name = "Sony";
-                $msg = "Hello, Welcome ";
-                echo apply_filters('fbs_ac_welcome_msg' , $msg, $your_name);
-            ?>
-            <div>
-                Twitter : <?php echo $twitter ?><br>
-                Facebook : <?php echo $facebook ?><br>
-                Bio : <?php echo $bio ?>
-            </div>
-        </div>
-    <?php
-
-    $author_info = ob_get_clean();
-
-    return $content . $author_info;
-}
-
-add_filter('the_content','fbs_author_contact_info');
-
-// our action hook testing
-function fbs_ac_bio_content_top_callback($name,$age){
-    $name = "Rony";
-    echo "<h3>My name is $name and my age is $age</h3>";
-}
-add_action('fbs_ac_bio_content_top' , 'fbs_ac_bio_content_top_callback',10,2);
-
-// testing our filter hooks
-function fbs_ac_welcome_msg_callback($msg,$your_name){
-    $your_name = "Gablim";
-    return $msg . $your_name;
-}
-add_filter('fbs_ac_welcome_msg','fbs_ac_welcome_msg_callback',10,2);
+ }
